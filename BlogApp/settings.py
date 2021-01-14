3623b23efa49e27c9af60b8bdfa8ff2fdd59575e
Nicholas <nickirabua@gmail.com>
2021-01-14 09:36:14 +0300

    adding email configurations

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -119,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'munyuanickie@gmail.com'
+EMAIL_HOST_PASSWORD = '#Munyu@me.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
